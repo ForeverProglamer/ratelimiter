aac408f1ba7225d464cc1a539cadde754b449c1c
ForeverProglamer <jet.shustriy@gmail.com>
2024-08-08 23:30:52 +0300

    feat: wake up tasks for retrying
    Now if we have requests remaining and we can do the retrying in the same
    time window we wake up more tasks to do it.

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import asyncio
 import logging
 import time
-from collections import defaultdict
+from collections import defaultdict, Counter
 from dataclasses import dataclass, field
 from datetime import datetime, UTC, timedelta
 from enum import StrEnum
 
+from tenacity import before_log, retry, stop_after_attempt, wait_fixed, retry_if_exception_type
+
 datefmt = "%H:%M:%S"
 logging.basicConfig(format="%(asctime)s %(message)s", datefmt=datefmt, level=logging.INFO)
 logging.Formatter.converter = time.gmtime
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -57,6 +60,7 @@ class HttpClient:
         self.host_to_requests_info: dict[str, HostRequestsInfo] = defaultdict(lambda: HostRequestsInfo())
         self.run_bg_task = True
         self.bg_task = asyncio.create_task(self._control_tasks_notification())
+        self.concurrent_requests: dict[str, int] = Counter()
 
     async def _control_tasks_notification(self) -> None:
         while self.run_bg_task:
@@ -74,6 +78,20 @@ class HttpClient:
                         )
                         requests_info.stage = Stage.SENDING_CONCURRENT_REQUESTS
                         requests_info.condition.notify(requests_info.ratelimit.limit - 1)  # type: ignore
+                elif requests_info.stage == Stage.SENDING_CONCURRENT_REQUESTS and (
+                    requests_info.requests_sent_in_time_window + self.concurrent_requests[host] 
+                    < requests_info.ratelimit.limit  # type: ignore
+                ) and requests_info.incoming_requests > 0:
+                    async with requests_info.condition:
+                        tasks_to_notify = (
+                            requests_info.ratelimit.limit -   # type: ignore
+                            (requests_info.requests_sent_in_time_window + self.concurrent_requests[host])
+                        )
+                        logging.info(
+                            f"{host} | notifying additional {tasks_to_notify} tasks, "  # type: ignore
+                            f"reset={requests_info.ratelimit.reset.strftime(datefmt)}"  # type: ignore
+                        )
+                        requests_info.condition.notify(tasks_to_notify)  # type: ignore
                 elif (
                     requests_info.stage == Stage.WAITING_FOR_RESET and
                     now >= requests_info.ratelimit.reset  # type: ignore
@@ -83,7 +101,13 @@ class HttpClient:
 
             await asyncio.sleep(0.3)
 
-    async def request(self, url: str, ratelimit: RateLimit) -> None:
+    @retry(
+        stop=stop_after_attempt(2+1),
+        retry=retry_if_exception_type(ZeroDivisionError),
+        wait=wait_fixed(0.5),
+        before=before_log(logger, logging.DEBUG)
+    )
+    async def request(self, url: str, ratelimit: RateLimit, raise_error: bool = False, retryable_error: bool = True) -> None:
         host, id_ = url.split(" ")
         requests_info = self.host_to_requests_info[host]
         
@@ -92,36 +116,51 @@ class HttpClient:
             requests_info.incoming_requests += 1
             await requests_info.condition.wait()
         requests_info.incoming_requests -= 1
+        self.concurrent_requests[host] += 1
 
         if requests_info.stage == Stage.SEND_ONE_REQUEST:
             requests_info.stage = Stage.SENDING_ONE_REQUEST
 
-        await self._send_request(url, ratelimit)
+        try:
+            await self._send_request(url, ratelimit, raise_error, retryable_error)
+        except Exception as error:
+            logging.error(f"{url=}, {error=}")
+            raise
+        finally:
+            self.concurrent_requests[host] -= 1
+            requests_info.requests_sent_in_time_window += 1
 
-        requests_info.requests_sent_in_time_window += 1
-
-        now = datetime.now(UTC)
-        if requests_info.stage == Stage.SENDING_ONE_REQUEST:
-            requests_info.ratelimit = ratelimit
-            if (
+            now = datetime.now(UTC)
+            if requests_info.stage == Stage.SENDING_ONE_REQUEST and requests_info.ratelimit:
+                if (
+                    now >= requests_info.ratelimit.reset or
+                    requests_info.requests_sent_in_time_window == requests_info.ratelimit.limit
+                ):
+                    requests_info.stage = Stage.WAITING_FOR_RESET
+                elif now < requests_info.ratelimit.reset and requests_info.incoming_requests >= 1:
+                    requests_info.stage = Stage.SEND_CONCURRENT_REQUESTS
+                elif now < requests_info.ratelimit.reset and requests_info.incoming_requests == 0:
+                    requests_info.stage = Stage.SEND_ONE_REQUEST
+            elif requests_info.stage == Stage.SENDING_CONCURRENT_REQUESTS and requests_info.ratelimit and (
                 now >= requests_info.ratelimit.reset or
                 requests_info.requests_sent_in_time_window == requests_info.ratelimit.limit
             ):
                 requests_info.stage = Stage.WAITING_FOR_RESET
-            elif now < requests_info.ratelimit.reset and requests_info.incoming_requests >= 1:
-                requests_info.stage = Stage.SEND_CONCURRENT_REQUESTS
-            elif now < requests_info.ratelimit.reset and requests_info.incoming_requests == 0:
-                requests_info.stage = Stage.SEND_ONE_REQUEST
-        elif requests_info.stage == Stage.SENDING_CONCURRENT_REQUESTS and requests_info.ratelimit and (
-            now >= requests_info.ratelimit.reset or
-            requests_info.requests_sent_in_time_window == requests_info.ratelimit.limit
-        ):
-            requests_info.stage = Stage.WAITING_FOR_RESET
 
-    async def _send_request(self, url: str, ratelimit: RateLimit) -> None:
+    async def _send_request(self, url: str, ratelimit: RateLimit, raise_error: bool = False, retryable_error: bool = True) -> None:
+        host, _ = url.split(" ")
+        requests_info = self.host_to_requests_info[host]
+
         logging.info(f"Sending request to {url}, {ratelimit=}...")
         await asyncio.sleep(1)
+        if requests_info.stage == Stage.SENDING_ONE_REQUEST:
+            requests_info.ratelimit = ratelimit
         logging.info(f"Response received for {url}!")
+
+        if raise_error:
+            if retryable_error:
+                raise ZeroDivisionError
+            raise RuntimeError
 
     async def close(self) -> None:
         self.run_bg_task = False
@@ -158,10 +197,25 @@ async def sequential_requests_single_host_run() -> None:
     logging.info(f"{client.host_to_requests_info=}\n")
 
 
+async def concurrent_requests_single_host_retry_run() -> None:
+    logging.info("Concurrent Requests Single Host Run | Retry")
+    client = HttpClient()
+    data = [(f"host-a {i+1}", ratelimit, i in (1, 2), i != 2) for i, ratelimit in enumerate(generate_server_response_headers(15)[:3])]
+    result = await asyncio.gather(
+        *[client.request(url, ratelimit, raise_error, retryable_error) for url, ratelimit, raise_error, retryable_error in data],
+        return_exceptions=True
+    )
+    logging.info(f"{result=}")
+    await client.close()
+    logging.info(f"{client.host_to_requests_info=}\n")
+
+
 async def main() -> None:
     await concurrent_requests_single_host_run()
     await concurrent_requests_multiple_hosts_run()
     await sequential_requests_single_host_run()
+
+    await concurrent_requests_single_host_retry_run()
 
 
 if __name__ == "__main__":
